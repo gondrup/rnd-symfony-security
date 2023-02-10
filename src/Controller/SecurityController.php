02243b07cddd676fd72b35da2befd53b1e8cb4cd
Adrian Harding <adrian@theaacars.com>
2023-02-10 17:26:09 +0000

    Add logout, add CSRF to login form, update home page

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,11 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HomeController extends AbstractController
+class SecurityController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
+    #[Route('/logout', name: 'app_logout', methods: ['GET'])]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig');
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
     }
 }
